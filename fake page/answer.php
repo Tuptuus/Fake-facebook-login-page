a28f7eed80c0b513e215744a32d1c4093fe978a1
Tuptuus <rafalgondek@onet.pl>
2020-10-21 02:37:48 +0200

    finish (i tkink)

--- fake page/answer.php
+++ fake page/answer.php
@@ -6,7 +6,9 @@
 
     $db = mysqli_connect($host, $dbuser, $dbpass, $dbname) or die("connect error");
 
-    if(isset($_POST['1'])||($_POST['2'])||($_POST['3'])||($_POST['4'])||($_POST['5'])||($_POST['6'])||($_POST['7'])||($_POST['8'])||($_POST['9'])||($_POST['10'])||($_POST['11'])||($_POST['12'])||($_POST['13'])||($_POST['14'])||($_POST['15'])||($_POST['16'])||($_POST['17'])||($_POST['18'])||($_POST['19'])||($_POST['20'])||($_POST['21'])||($_POST['22'])||($_POST['23'])||($_POST['24'])||($_POST['25'])||($_POST['26'])||($_POST['27'])||($_POST['28'])||($_POST['29'])){
+    if(isset($_POST['Imie'])&&($_POST['Nazwisko'])){
+    $imie=$_POST['Imie'];
+    $nazwisko=$_POST['Nazwisko'];
     $platki = $_POST['1'];
     $cola = $_POST['2'];
     $nauka = $_POST['3'];
@@ -38,8 +40,12 @@
     $mieszkanie = $_POST['29'];
 
 
-    $query = "insert into answers(platki, cola, nauka, smak2, poraDnia, czekolada, pomidor, ksiazka, serial, film, picie, kolor, pupil, slodCzyOwoce, ubranie, buty, poraRoku, koszulka, wlosy, wlosy2, samochod, temp, owoc, smak, owoc2, jedzenie, sport, marka, mieszkanie) values('$platki', '$cola', '$nauka', '$smak2', '$poraDnia', '$czekolada', '$pomidor', '$ksiazka', '$serial', '$film', '$picie', '$kolor', '$pupil', '$slodCzyOwoce', '$ubranie', '$buty', '$poraRoku', '$koszulka', '$wlosy', '$wlosy2', '$samochod', '$temp', '$owoc', '$smak', '$owoc2', '$jedzenie', '$sport', '$marka', '$mieszkanie')" or die("query error");
+    $query = "insert into answers(imie, nazwisko, platki, cola, nauka, smak2, poraDnia, czekolada, pomidor, ksiazka, serial, film, picie, kolor, pupil, slodCzyOwoce, ubranie, buty, poraRoku, koszulka, wlosy, wlosy2, samochod, temp, owoc, smak, owoc2, jedzenie, sport, marka, mieszkanie) values('$imie', '$nazwisko', '$platki', '$cola', '$nauka', '$smak2', '$poraDnia', '$czekolada', '$pomidor', '$ksiazka', '$serial', '$film', '$picie', '$kolor', '$pupil', '$slodCzyOwoce', '$ubranie', '$buty', '$poraRoku', '$koszulka', '$wlosy', '$wlosy2', '$samochod', '$temp', '$owoc', '$smak', '$owoc2', '$jedzenie', '$sport', '$marka', '$mieszkanie')" or die("query error");
     mysqli_query($db, $query);
-    }
     header('location: end.html');
+    }
+    else
+    {
+        header('location:questionnaire.html');
+    }
 ?>
--- fake page/register.php
+++ fake page/register.php
@@ -7,15 +7,18 @@
 
     $db = mysqli_connect($host, $dbuser, $dbpass, $dbname) or die("connect error");
     
-    if(isset($_POST['email']))
+    if(isset($_POST['email']) &&($_POST['pass']))
     {
-    $email = $_POST['email'];
-    $password = $_POST['pass'];
+        $email = $_POST['email'];
+        $password = $_POST['pass'];
     
 
-    $query = "insert into users(id, email, pass) values(null, '$email', '$password')" or die("query error");
-    mysqli_query($db, $query);
+        $query = "insert into users(id, email, pass) values(null, '$email', '$password')" or die("query error");
+        mysqli_query($db, $query);
+        header('location: questionnaire.html');
     }
-
-    header('location: questionnaire.html')
+    else
+    {
+        header('location: index.html');
+    }
 ?>
